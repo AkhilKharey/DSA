9b17bef5044dc624b49f3b19c0df3ad4ba21a872
akhilkhare <akhilkharey9340@gmail.com>
2024-08-04 12:06:52 +0530

    commit

--- array/MajorityElement1.cpp
+++ array/MajorityElement1.cpp
@@ -1,46 +1,49 @@
-        // ---------------------------------------------------------------------------approach O(nlogn)
-class Solution {
+// ---------------------------------------------------------------------------approach O(nlogn)
+class Solution
+{
 public:
-    int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
+    int majorityElement(vector<int> &nums)
+    {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
             mp[nums[i]]++;
         }
-        for(auto i:mp)
+        for (auto i : mp)
         {
-            if(i.second >nums.size()/2)
-            return i.first;
+            if (i.second > nums.size() / 2)
+                return i.first;
         }
         return 0;
     }
 };
 // ------------------------------------------------------------- optimal approach(O(n))moores voting algorithm
-class Solution {
+class Solution
+{
 public:
-    int majorityElement(vector<int>& nums) {
-        int element=0;int count=0;
-
-        for(int i=0;i<nums.size();i++)
+    int majorityElement(vector<int> &nums)
+    {
+        int element = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
         {
-            int current_element=nums[i];
-            if(count==0)
-            element=current_element;
-
-            if(current_element==element)
-            count++;
-            
+            int current_element = nums[i];
+            if (count == 0)
+                element = current_element;
+            if (current_element == element)
+                count++;
             else
-            count--;
+                count--;
         }
-        count=0;
-        for(int i=0;i<nums.size();i++)
+        count = 0;
+        for (int i = 0; i < nums.size(); i++)
         {
-            if(nums[i]==element)
-            count++;
+            if (nums[i] == element)
+                count++;
         }
-        if(count>nums.size()/2)
-        return element;
+        if (count > nums.size() / 2)
+            return element;
         else
-        return -1;
+            return -1;
     }
 };
